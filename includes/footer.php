23d882d1194a5502abd82ed20b399a82a566209c
Chang <trangkieu1724@gmail.com>
2025-11-15 19:33:09 +0700

    footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 		<br>
 		<p>"Ghi chú những gì bạn cần làm để không bỏ lỡ những nhiệm vụ quan trọng!"</p>
 		<p>&copy; Simple To-Do List App</p>
-	</footer>
+</footer>
 	</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
